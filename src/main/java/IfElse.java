c30683e401f48be64a09936370ea2a8925a22aee
Kyle Ochate <kyleochate@gmail.com>
2022-03-12 19:52:14 -0500

    Initial upload

---
+++ src/main/java/IfElse.java
@@ -0,0 +1,24 @@
+import java.io.Console;
+import java.util.Random;
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int myNum = 24;
+        Scanner numInput = new Scanner(System.in);
+
+        System.out.println("Let's compare numbers, pick a whole number. The number I pick is 24.");
+
+        System.out.println("Please input a number: ");
+
+        //int input
+        int num = numInput.nextInt();
+        if (myNum > num) {
+            System.out.println("My number is bigger than yours.");
+        }else if (myNum == num){
+            System.out.println("You copied my number, pick a different number and try again.");
+        }else{
+            System.out.println("Your number is bigger than mine. Congratulations.");
+        }
+    }
+}
---
+++ src/main/java/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        String fName = "Kyle";
+        int a = 12;
+        float b = 12.39f;
+        float v = a * b;
+        System.out.println(fName + " wanted to know what " + a + " x " + b + " equates to. Thankfully, Java can make this whole operation easy to figure out, the answer being");
+        System.out.println(v);
+    }
+
+
+}
